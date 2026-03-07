d4e02658430eae05487f9e321fe45cc42812bd9c
Joseph Earl Weiner <josephearlweiner83@gmail.com>
2026-03-06 19:35:27 -0600

    added foreign key restraint for tune_type_id

--- testdatabase.sql
+++ testdatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2026 at 01:54 AM
+-- Generation Time: Mar 07, 2026 at 02:31 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,10 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `tunebook`
 --
-
-CREATE DATABASE IF NOT EXISTS tunebook;
-
-USE tunebook;
 
 -- --------------------------------------------------------
 
@@ -53,9 +49,9 @@ INSERT INTO `composers` (`composer_id`, `composer_name`) VALUES
 CREATE TABLE `tunes` (
   `tune_id` int(25) NOT NULL,
   `tune_title` varchar(100) NOT NULL,
-  `tune_type` int(20) NOT NULL,
+  `tune_type_id` int(20) NOT NULL,
   `author_id` int(25) NOT NULL,
-  `composer_id` int(25) NOT NULL,
+  `composer_id` int(25) DEFAULT NULL,
   `metre` varchar(5) NOT NULL,
   `default_note_length` varchar(5) NOT NULL,
   `key` varchar(15) NOT NULL,
@@ -68,11 +64,10 @@ CREATE TABLE `tunes` (
 -- Dumping data for table `tunes`
 --
 
-INSERT INTO `tunes` (`tune_id`, `tune_title`, `tune_type`, `author_id`, `composer_id`, `metre`, `default_note_length`, `key`, `tune_body`, `audio`, `video`) VALUES
-(7, 'Green Fields of America', 1, 33, 0, '4/4', '1/8', 'G', 'c2ec B2dB | AGAB AGEF| ~G3E DEGB | AGAB AGEG |\nc2ec B2dB | AGAB AGEF | ~G3E DEGA|BGAF G3z :||\nGABc d2ef | gage dBGB | c2ec B2dB | AGAB AGE2 |\n~g3d efge | dcBG AGEF | ~G3E DEGA | BGAF G3z :||\n', 'None', 'None'),
-(8, 'Barr Na Cuille', 1, 33, 0, '4/4', '1/8', 'D', 'G|:FAAF GBdg|fAdf dBAG|FA (3ABA GBdg|fdef d3 G|\nFA (3AAA Bddg|fAdf efdA|FA (3AAA GBdg|fdef d3 g|\n|:faaf gbbg|faag gfed|faaf gbag|fdef d3 g\nfaaf gbbg|faag gfed|fdfa (3bbb bg|fdef dBAG|]', 'None', 'None'),
-(9, 'Ruined Old Cottage in the', 1, 33, 0, '4/4', '1/8', 'E dorian', '|:B2 AF D2 FA|B~A3 (3Bcd ef|dBAF DEFA|BdAF FEEA:|\n|B2 ed BdAd|(3Bcd ef gfed|B2 ed BdAd|(3Bcd AF FEEA|\n|ABed BdAd|(3Bcd ef g2 ga|bg (3agf gfed|BdAF FEEA|', 'None', 'None'),
-(10, 'Ruined Old Cottage in the Glen', 1, 33, 0, '4/4', '1/8', 'E dorian', '|:B2 AF D2 FA|B~A3 (3Bcd ef|dBAF DEFA|BdAF FEEA:|\n|B2 ed BdAd|(3Bcd ef gfed|B2 ed BdAd|(3Bcd AF FEEA|\n|ABed BdAd|(3Bcd ef g2 ga|bg (3agf gfed|BdAF FEEA|', 'None', 'None');
+INSERT INTO `tunes` (`tune_id`, `tune_title`, `tune_type_id`, `author_id`, `composer_id`, `metre`, `default_note_length`, `key`, `tune_body`, `audio`, `video`) VALUES
+(7, 'Green Fields of America', 1, 33, NULL, '4/4', '1/8', 'G', 'c2ec B2dB | AGAB AGEF| ~G3E DEGB | AGAB AGEG |\nc2ec B2dB | AGAB AGEF | ~G3E DEGA|BGAF G3z :||\nGABc d2ef | gage dBGB | c2ec B2dB | AGAB AGE2 |\n~g3d efge | dcBG AGEF | ~G3E DEGA | BGAF G3z :||\n', 'None', 'None'),
+(8, 'Barr Na Cuille', 1, 33, NULL, '4/4', '1/8', 'D', 'G|:FAAF GBdg|fAdf dBAG|FA (3ABA GBdg|fdef d3 G|\nFA (3AAA Bddg|fAdf efdA|FA (3AAA GBdg|fdef d3 g|\n|:faaf gbbg|faag gfed|faaf gbag|fdef d3 g\nfaaf gbbg|faag gfed|fdfa (3bbb bg|fdef dBAG|]', 'None', 'None'),
+(10, 'Ruined Old Cottage in the Glen', 1, 33, NULL, '4/4', '1/8', 'E dorian', '|:B2 AF D2 FA|B~A3 (3Bcd ef|dBAF DEFA|BdAF FEEA:|\n|B2 ed BdAd|(3Bcd ef gfed|B2 ed BdAd|(3Bcd AF FEEA|\n|ABed BdAd|(3Bcd ef g2 ga|bg (3agf gfed|BdAF FEEA|', 'None', 'None');
 
 -- --------------------------------------------------------
 
@@ -135,7 +130,7 @@ ALTER TABLE `tunes`
   ADD PRIMARY KEY (`tune_id`),
   ADD KEY `author_id` (`author_id`),
   ADD KEY `composer_id` (`composer_id`),
-  ADD KEY `tune_type` (`tune_type`);
+  ADD KEY `tune_type_id` (`tune_type_id`);
 
 --
 -- Indexes for table `tune_types`
@@ -187,7 +182,8 @@ ALTER TABLE `users`
 -- Constraints for table `tunes`
 --
 ALTER TABLE `tunes`
-  ADD CONSTRAINT `tunes_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `tunes_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `tunes_ibfk_2` FOREIGN KEY (`tune_type_id`) REFERENCES `tune_types` (`tune_type_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
